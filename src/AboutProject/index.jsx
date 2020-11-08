bcc4454b41a1e2cc7480585ab9693114a5d33977
Jana Stastna <j.machkova@gmail.com)>
2020-11-08 08:53:50 +0100

    komponenta AboutProject bez CSS

--- src/AboutProject/index.jsx
+++ src/AboutProject/index.jsx
@@ -1,4 +1,31 @@
 import React from 'react';
-import './style.css'
+import './style.css';
 
-const AboutProject
+export const AboutProject = () => {
+  return (
+    <>
+      <section className="aboutProject">
+        <h1>O projektu</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, vero
+          corrupti. Fugit incidunt alias, quasi repellat assumenda debitis iusto
+          doloremque ea impedit ut aliquid excepturi, illum minima est rem
+          repudiandae!
+        </p>
+        <div class="image"></div>
+        <h2>Pro firmy</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam
+          excepturi minus eveniet architecto facilis culpa facere quod, vero
+          commodi! Nobis.
+        </p>
+        <h2>Pro školy</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+          adipisci perferendis quaerat cumque nihil obcaecati ipsa quisquam
+          suscipit sed odio?
+        </p>
+      </section>
+    </>
+  );
+};
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
 import './index.html';
-import './style.css'
+import './style.css';
 import { Header } from './Header/index.jsx';
+import {AboutProject} from "./AboutProject/index.jsx"
 
 const App = () => {
-  return <Header />;
+  return (
+    <>
+  <Header />
+  <AboutProject/>
+  </>
+  )
 };
 
 render(<App />, document.querySelector('#app'));

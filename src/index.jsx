1a423a73c401abb2d4c38d7fcf7ae329c591412c
Zuzana Belaskova <z.belaskova@gmail.com>
2020-11-08 13:35:36 +0100

    About us component created

--- src/index.jsx
+++ src/index.jsx
@@ -4,12 +4,14 @@ import './index.html';
 import './style.css';
 import { Header } from './Header/index.jsx';
 import { AboutProject } from './AboutProject/index.jsx';
+import { AboutUs } from './AboutUs/index.jsx';
 
 const App = () => {
   return (
     <>
       <Header />
       <AboutProject />
+      <AboutUs />
     </>
   );
 };

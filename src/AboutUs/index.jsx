c93965a1a9f93d94e4af6ef7ae680bd5d57e1929
Jana Stastna <j.machkova@gmail.com)>
2020-11-09 22:32:51 +0100

    CSS stylovani, blizime se ke konci

--- src/AboutUs/index.jsx
+++ src/AboutUs/index.jsx
@@ -7,28 +7,34 @@ export const AboutUs = () => {
       <h1 className="titleAbout">O nás</h1>
       <div className="About">
         <div className="AboutZuzana">
-          <h3>Zuzana</h3>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel sunt
-            reiciendis possimus quidem voluptate aut! Fuga accusamus alias
-            libero neque, officiis est iure reiciendis porro eos distinctio,
-            perspiciatis ipsum voluptates possimus quo ipsa a. Ab quisquam,
-            officia accusantium incidunt repellendus, tempore distinctio, itaque
-            cum necessitatibus qui minima pariatur omnis quas animi repellat
-            facere! Ipsum laboriosam voluptatum dolore, quasi ipsa qui?
-          </p>
+          <div className="fotoJmenoZuzana">
+            <h3>Zuzana</h3>
+            <div className="fotoZuzana"></div>
+          </div>
+          <div className="parZuzana">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel sunt
+              reiciendis possimus quidem voluptate aut! Fuga accusamus alias
+              libero neque, officiis est iure reiciendis porro eos distinctio,
+              perspiciatis ipsum voluptates possimus quo ipsa a. Ab quisquam,
+              offic
+            </p>
+          </div>
         </div>
+
         <div className="AboutJana">
-          <h3>Jana</h3>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint
-            nostrum dicta aspernatur! Ipsum minus soluta, vero quis, pariatur,
-            temporibus voluptatem a corrupti sit similique ex delectus sunt
-            optio magnam nobis? Voluptatem illo omnis fugiat corporis, molestias
-            hic et doloribus quam, rerum quia delectus soluta expedita molestiae
-            voluptates optio animi ea! Incidunt soluta reprehenderit sapiente
-            placeat, et molestiae similique saepe voluptates.
-          </p>
+          <div className="fotoJmenoJana">
+            <h3>Jana</h3>
+            <div className="fotoJana"></div>
+          </div>
+          <div className="parJana">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint
+              nostrum dicta aspernatur! Ipsum minus soluta, vero quis, pariatur,
+              temporibus voluptatem a corrupti sit similique ex delectus sunt
+              optio magnam nobis? Voluptatem illo omni
+            </p>
+          </div>
         </div>
       </div>
     </div>
--- src/Footer/index.jsx
+++ src/Footer/index.jsx
@@ -5,8 +5,7 @@ export const Footer = (props) => {
   return (
     <>
       <footer className="sectionFooter">
-        <div className="logo"></div>
-        <div className="DA">Digitální akademie 2020</div>
+        <div className="DA">CZECHITAS: Digitální akademie 2020</div>
       </footer>
     </>
   );

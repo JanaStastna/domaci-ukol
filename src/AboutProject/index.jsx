aa37d47027fc2c60048a6807055e1f4295f77e0c
Jana Stastna <j.machkova@gmail.com)>
2020-11-10 14:05:15 +0100

    BEM AboutProject

--- src/AboutProject/index.jsx
+++ src/AboutProject/index.jsx
@@ -11,25 +11,27 @@ export const AboutProject = () => {
   return (
     <>
       <section id="aboutProject" className="aboutProject">
-        <div className="section1">
-          <div className="sectionProject">
-            <h1 className="mainTitle">O projektu</h1>
-            <p className="parAboutProject">
-              Pandemie koronaviru přenusula studium do online prostředí, ale
-              mnoho žáků a studentů nemá vyhovující podmínky a každým dnem jim
-              tak utíká část výuky. Tuto zrátu budou po návratu do škol stěží
-              dohánět. Pojďme společně pomoci tam, kde je to potřeba.
+        <div className="aboutProject__section1">
+          <div className="aboutProject__section1--mainInfo">
+            <h1 className="aboutProject__section1--mainInfo--title">
+              O projektu
+            </h1>
+            <p className="aboutProject__section1--mainInfo--par">
+              Pandemie koronaviru přenusula studium do online prostředí. Mnoho
+              žáků a studentů ale nemá vyhovující podmínky a každým dnem jim tak
+              utíká část výuky. Tuto zrátu budou po návratu do škol stěží
+              dohánět. Pojďme společně pomoci! Dostaňme nepotřebnou IT techniku
+              ke studentům, kteří ji potřebují.
             </p>
           </div>
-          <div className="aboutProject-image"></div>
-          <div className="message">
+          <div className="aboutProject__section1--message">
             Nedarujete jen počítač, darujete vzdělání!
           </div>
         </div>
-        <div className="section2">
-          <div className="forCompanies">
-            <h3 className="title">Pro firmy</h3>
-            <p className="parCompanies">
+        <div className="aboutProject__section2">
+          <div className="aboutProject__section2--forCompanies">
+            <h3 className="aboutProject__section2--title">Pro firmy</h3>
+            <p className="aboutProject__section2--par">
               Máte v práci nevyužitou IT techniku a lámete si hlavu, co s ní?
               Pojďte s námi podpořit školáky, kteří nemají v dobách online
               vzdělávání snadný přístup k počítači. Nevyužitou IT techniku
@@ -37,9 +39,9 @@ export const AboutProject = () => {
               potřebuje.
             </p>
           </div>
-          <div className="forSchools">
-            <h3 className="title">Pro školy</h3>
-            <p className="parSchools">
+          <div className="aboutProject__section2--forSchools">
+            <h3 className="aboutProject__section2--title">Pro školy</h3>
+            <p className="aboutProject__section2--par">
               Pokud zastupujete školu nebo dětský domov a víte o studentovi,
               kterému bychom mohli pomoci, dejte nám vědět a vyplňte krátký
               formulář. Děkujeme!
